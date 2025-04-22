d49639bc711f84b8f56863a971f457001da3ffdb
GGsuii99 <123018@combertonvc.org>
2025-04-22 17:33:55 +0100

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,8 +4,8 @@ const CTA = () => {
   return (
     <section className='cta'>
       <p className='cta-text'>
-        Have a project in mind? <br className='sm:block hidden' />
-        Let’s build something together!
+        Want to talk? <br className='sm:block hidden' />
+        
       </p>
       <Link to='/contact' className='btn'>
         Contact
